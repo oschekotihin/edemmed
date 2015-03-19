79687d285d751fe5c645d4921741f493cafefbdf
oschekotihin <Oleg_9l@mail.ru>
2015-03-19 14:59:00 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ $(function() {
  
     //build slider
     var scrollbar = $( ".scroll-bar" ).slider({
+
+      
+     
+
       slide: function( event, ui ) {
         if ( scrollContent.width() > scrollPane.width() ) {
           scrollContent.css( "margin-left", Math.round(
@@ -13,34 +17,18 @@ $(function() {
         } else {
           scrollContent.css( "margin-left", 0 );
         }
-      }
+      }, 
+      range: "min",
     });
  
     //append icon to handle
-    var handleHelper = scrollbar.find( ".ui-slider-handle" )
-    .mousedown(function() {
-      scrollbar.width( handleHelper.width() );
-    })
-    .mouseup(function() {
-      scrollbar.width( "100%" );
-    })
-    .append( "<span class='ui-icon ui-icon-grip-dotted-vertical'></span>" )
-    .wrap( "<div class='ui-handle-helper-parent'></div>" ).parent();
+  
  
     //change overflow to hidden now that slider handles the scrolling
     scrollPane.css( "overflow", "hidden" );
  
     //size scrollbar and handle proportionally to scroll distance
-    function sizeScrollbar() {
-      var remainder = scrollContent.width() - scrollPane.width();
-      var proportion = remainder / scrollContent.width();
-      var handleSize = scrollPane.width() - ( proportion * scrollPane.width() );
-      scrollbar.find( ".ui-slider-handle" ).css({
-        width: handleSize,
-        "margin-left": -handleSize / 2
-      });
-      handleHelper.width( "" ).width( scrollbar.width() - handleSize );
-    }
+    
  
     //reset slider value based on scroll content position
     function resetValue() {
